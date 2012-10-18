fbda7155834de8b336e1e739d9790b0ee844685a
windless <windlessg@gmail.com>
2012-10-18 18:03:06 +0800

    Finish home page

---
+++ spec/requests/home_pages_spec.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe "HomePages" do
+  subject { page }
+
+  before { visit root_path }
+
+  it { should have_selector('h1', text: '欢迎') }
+  it { should have_selector('#logo', text: 'Jobs') }
+  it { should have_link('主页', href: root_path) }
+  it { should have_link('注册') }
+  it { should have_link('登录') }
+end
